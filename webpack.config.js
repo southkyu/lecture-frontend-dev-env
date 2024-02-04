4f605eac03174e62275146e71a96d9c0ca6cf6fb
southkyu <southkyu@gmail.com>
2024-02-04 17:32:29 +0900

    ⚡️ 로더 설정

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,39 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/app.js'
+    main: "./src/app.js",
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve('./dist'),
-  }
-}
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      // {
+      //   test: /\.(png|jpg|gif|svg)$/, // .png 확장자로 마치는 모든 파일
+      //   loader: "file-loader",
+      //   options: {
+      //     publicPath: "./dist/", // prefix를 아웃풋 경로로 지정
+      //     name: "[name].[ext]?[hash]", // 파일명 형식
+      //   },
+      // },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        use: {
+          loader: "url-loader", // url 로더를 설정한다
+          options: {
+            publicPath: "./dist/", // file-loader와 동일
+            name: "[name].[ext]?[hash]", // file-loader와 동일
+            limit: 10000, // 5kb 미만 파일만 data url로 처리
+          },
+        },
+      },
+    ],
+  },
+};
